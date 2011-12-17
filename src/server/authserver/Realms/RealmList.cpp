19a14e357a09a26b2fedcbd7b42731c357523c40
Cannella <en.ri.co.ca@hotmail.it>
2011-12-17 01:41:35 +0100

    FannNuBucchinEMU now is up : )

--- src/server/authserver/Realms/RealmList.cpp
+++ src/server/authserver/Realms/RealmList.cpp
@@ -5,6 +5,8 @@
  *
  * Copyright (C) 2010-2011 Project SkyFire <http://www.projectskyfire.org/>
  *
+ * Copyright (C) 2010-2011 Project FammNuBucchinEMU <http://www.fammnubucchin.it/>
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
